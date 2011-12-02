89449261f86fa0065f45991bae40c6c021d63bd0
Christoph S. Ackermann <info@acki.be>
2011-12-02 08:41:09 +0100

    Remove not needed ticks

--- SQLBackupToSamba.sh
+++ SQLBackupToSamba.sh
@@ -7,7 +7,7 @@ mkdir $MOUNTFOLDER
 
 mysqldump -u$SQLUSER -p$SQLPASS --all-databases > $TEMPFOLDER/backup.sql
 
-mount -t cifs $SMBFOLDER $MOUNTFOLDER -o user='$SMBUSER',password='$SMBPASS'
+mount -t cifs $SMBFOLDER $MOUNTFOLDER -o user=$SMBUSER,password=$SMBPASS
 
 ls $MOUNTFOLDER
 
