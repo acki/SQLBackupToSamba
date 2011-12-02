c91a14c85492f5ce16a95a59867b635611e24cc5
Christoph S. Ackermann <info@acki.be>
2011-12-02 08:29:25 +0100

    Added mount and ls

--- SQLBackupToSamba.sh
+++ SQLBackupToSamba.sh
@@ -6,3 +6,7 @@ mkdir $TEMPFOLDER
 mkdir $MOUNTFOLDER
 
 mysqldump -u$SQLUSER -p$SQLPASS --all-databases > $TEMPFOLDER/backup.sql
+
+mount -t cifs $SMBFOLDER $MOUNTFOLDER -o user=$SMBUSER,password=$SMBPASS
+
+ls $MOUNTFOLDER
